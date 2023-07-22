a69222d3ed12616351593c5d01ae7bddd515082a
Nícolas <nicolasdamotaarruda@hotmail.com>
2023-07-22 01:50:07 -0300

    Adicionando comentários no código e tentando simplificar algumas coisas

--- Assets/Scripts/CordeiroScript.cs
+++ Assets/Scripts/CordeiroScript.cs
@@ -3,78 +3,102 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LambScript : MonoBehaviour
-{   
-    private int inputX, velocidade = 60, flip = 0, altura = 0;
+{
+    private int inputX, velocidade = 60, altura = 0;
     private float distancia;
+    private bool pulo = false, olhandoEsquerda=false;
     public BoxCollider2D hitbox1;
     public Animator animator;
     private Vector2 vector2;
-    public GameObject chão;
-    private bool pulo = false;
+    public GameObject chao;
 
-    void Awake(){
+    void Awake()
+    {
         hitbox1 = gameObject.GetComponent<BoxCollider2D>();
     }
 
-    void Update(){
-        distancia = (int)transform.position.y - (int)chão.transform.position.y;
+    void Update()
+    {
+        distancia = (int)transform.position.y - (int)chao.transform.position.y;
         movimento();
     }
-    
-    void movimento(){
-        if(Input.GetKey(KeyCode.Space) && (distancia < 40) && (!pulo)){
+
+    void movimento()
+    {
+        /*Fazendo a funcionalidade do pulo*/
+        if (Input.GetKey(KeyCode.Space) && (distancia < 40) && (!pulo))
+        {
             altura += 10;
-        }else if((distancia <= 25)){
+        }
+        else if ((distancia <= 25))
+        {
             altura = 0;
             pulo = false;
-        }else if((distancia >= 40)){
+        }
+        else if ((distancia >= 40))
+        {
             altura -= 5;
             pulo = true;
         }
 
-        if(Input.GetKey("left")){
-            if(flip == 0){
-                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-                flip = 1;
+        /*Fazendo a funcionalidade de andar para a esquerda e direita*/
+        if (Input.GetKey("left"))
+        {
+            if (!olhandoEsquerda)    /*Rotacionando o personagem*/
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                olhandoEsquerda = true;
             }
             inputX = -1;
             velocidade = 60;
-            if(Input.GetKey(KeyCode.LeftShift)){
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
                 velocidade = 150;
-            }else{
+            }
+            else
+            {
                 velocidade = 60;
             }
-
-        }else if(Input.GetKey("right")){
-            if(flip == 1){
-                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-                flip = 0;
-            }
-            inputX = 1;
-            velocidade = 2;
-            if(Input.GetKey(KeyCode.LeftShift)){
-                velocidade = 150;
-            }else{
-                velocidade = 60;
-            }
-
-        }else{
-            velocidade = 0;
-            inputX = 0;
 
         }
+        else if (Input.GetKey("right"))
+        {
+            if (olhandoEsquerda)       /*Rotacionando o personagem*/
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                olhandoEsquerda = false;
+            }
+            inputX = 1;
+            velocidade = 60;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                velocidade = 150;
+            }
+            else
+            {
+                velocidade = 60;
+            }
 
-        if(Input.GetKey(KeyCode.Z)){
+        }
+        else
+        {
+            velocidade = 0;
+            inputX = 0;
+        }
+
+        /*Fazendo a funcionalidade de hitbox e vida*/
+        if (Input.GetKey(KeyCode.Z))
+        {
             hitbox1.enabled = true;
-        }else{
+        }
+        else
+        {
             hitbox1.enabled = false;
         }
 
         animator.SetFloat("Velocidade", velocidade);
-
         transform.position = new Vector2(transform.position.x + inputX * velocidade * Time.deltaTime, transform.position.y + altura * Time.deltaTime);
         vector2 = transform.position;
     }
 
 }
-
